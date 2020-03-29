3e67262aa50f613a212718166ac42e49c3b6e95f
C0der-X <59411142+C0der-X@users.noreply.github.com>
2020-03-29 14:27:53 +0300

    Update spider.py

--- lib/spider.py
+++ lib/spider.py
@@ -10,21 +10,21 @@ def extractLinksFrom(url):
 	response = request.request(url)
 	return re.findall(r'(?:href=")(.*?)"', response.content.decode('utf-8', 'ignore'))
 def spider(url):
-	try:
-		hrefLinks = extractLinksFrom(url)
+	hrefLinks = extractLinksFrom(url)
 
-		for link in hrefLinks:
-			link = urljoin(url, link)
+	for link in hrefLinks:
+		link = urljoin(url, link)
 
-			if "#" in link:
-				link = link.split("#")[0]
-			if (url in link) and (link not in targetLinks):
-				targetLinks.append(link)
-				print(Fore.RED+"="*30)
-				print(Fore.RED+"| ",Fore.GREEN+"[+] Found URL: "+link)
-				print(Fore.RED+"="*30)
+		if "#" in link:
+			link = link.split("#")[0]
+		if (url in link) and (link not in targetLinks):
+			targetLinks.append(link)
+			print(Fore.RED+"="*30)
+			print(Fore.RED+"| ",Fore.GREEN+"[+] Found URL: "+link)
+			print(Fore.RED+"="*30)
+			try:
 				spider(link)
-		return targetLinks
-	except KeyboardInterrupt:
-		return targetLinks
+			except KeyboardInterrupt:
+				return targetLinks
+				break
 
