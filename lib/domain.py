3c0dfa58db7200dae5f78baccd4e0bf80d6f1d70
C0der-X <59411142+C0der-X@users.noreply.github.com>
2020-03-30 19:16:24 +0000

    Update domain.py

--- lib/domain.py
+++ lib/domain.py
@@ -25,7 +25,7 @@ class Domain:
 						print("\n"+Fore.BLUE+"="*30)
 						print(Fore.BLUE+"| "+Fore.GREEN+"[+] Discovered URL: "+Fore.CYAN+adress)
 						print(Fore.BLUE+"="*30)
-					print(Fore.RED+"\r[-] Tried: "+Fore.YELLOW+adress+"                               ",end="")
+					print(Fore.RED+"\r[-] Tried: "+Fore.YELLOW+adress+"                  ",end="")
 			return self.writer(urls)
 		except FileNotFoundError:
 			print("'4Eyes' klasöründe değilsiniz. Lütfen belirtilen klasöre girip programı tekrar çalıştırınız")
